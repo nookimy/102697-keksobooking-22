5b8f397af8b75381a6c8cf88f0a1b5a070994c92
Alina <alina@onmaster.ru>
2021-02-03 12:20:01 +0300

    Изменяет условия функций вычисления случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,35 +1,27 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно. Ссылка на источник https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
 const getRandomInt = function (min, max) {
-  if (min > max && min <= 0) {
-    alert('Значение «от» должно быть больше или равно значению «до» и положительным');
-  } if (min > max && min > 0) {
-    alert('Значение «от» должно быть больше или равно значению «до»');
-  } if (min <= 0) {
-    alert('Диапазон может быть только положительный')
-  } if ((max - min) < 1 && !Number.isInteger(min)) {
-    alert('В данном диапазоне нет целых чисел')
-  } if (min == max && Number.isInteger(min)) {
-    return min;
+  if (min < 0) {
+    alert('Диапазон чисел должен быть положительным');
+  } if (max < min || max == min) {
+    alert('Значение «от» должно быть больше значению «до»');
   }
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomInt();
+alert(getRandomInt(100.46, 200.46));
+
+
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
 const getRandomFloat = function (min, max, decimal = 2) {
-  if (min > max && min <= 0) {
-    alert('Значение «от» должно быть больше или равно значению «до» и положительным');
-  } if (min > max && min > 0) {
-    alert('Значение «от» должно быть больше или равно значению «до»');
-  } if (min <= 0) {
-    alert('Диапазон может быть только положительный')
-  } if (min == max) {
-    return (min).toFixed(decimal);
+  if (min < 0) {
+    alert('Диапазон чисел должен быть положительным');
+  } if (max < min || max == min) {
+    alert('Значение «от» должно быть больше значению «до»');
   }
   return +((Math.random() * (max - min) + min).toFixed(decimal));
 }
 
-getRandomFloat();
+alert(getRandomFloat(100.46, 200.46));
