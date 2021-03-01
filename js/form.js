3d2f66a2974c5b6e7baeda33a03916ec0a40b84b
Alina <alina@onmaster.ru>
2021-03-01 12:03:36 +0300

    Зависимость количества комнат от количества мест

--- js/form.js
+++ js/form.js
@@ -25,6 +25,8 @@ const elementsForm = {
   checkin: adForm.querySelector('#timein'),
   checkout: adForm.querySelector('#timeout'),
   address: adForm.querySelector('#address'),
+  roomNumber: adForm.querySelector('#room_number'),
+  guestNumber: adForm.querySelector('#capacity'),
 };
 
 // Валидация заголовка
@@ -74,10 +76,55 @@ elementsForm.checkout.addEventListener('change', () => {
   elementsForm.checkin.value = elementsForm.checkout.value;
 });
 
+// Количество комнат и количество мест
+const noGuests = {
+  value: 100,
+  text: 'не для гостей',
+};
+
+const oneGuests = {
+  value: 1,
+  text: 'для 1 гостя',
+};
+
+const twoGuests = {
+  value: 2,
+  text: 'для 2 гостей',
+};
+
+const threeGuests = {
+  value: 3,
+  text: 'для 3 гостей',
+};
+
+const options = {
+  100: [noGuests],
+  1: [oneGuests],
+  2: [oneGuests, twoGuests],
+  3: [oneGuests, twoGuests, threeGuests],
+};
+
+const getOptions = function (guests) {
+  for (let i = 0; i < guests.length; i++) {
+    const option = document.createElement('option');
+    option.value = guests[i].value;
+    option.innerHTML = guests[i].text;
+    elementsForm.guestNumber.appendChild(option);
+  }
+};
+
+// Зависимость количества комнат от количества мест
+elementsForm.roomNumber.addEventListener('change', function () {
+  const roomNumberValue = elementsForm.roomNumber.value;
+  elementsForm.guestNumber.value = (roomNumberValue === '100') ? '0' : roomNumberValue;
+
+  while (elementsForm.guestNumber.firstChild) {
+    elementsForm.guestNumber.removeChild(elementsForm.guestNumber.firstChild);
+  }
+
+  getOptions(options[roomNumberValue]);
+});
+
 inactiveForm();
-
-console.log(elementsForm.price.min);
-
-
 
 export {elementsForm, activeForm}
